27d6bc3d5494152ca105f0eeab3eada1ea2e0d87
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-17 15:42:00 -0700

    Fixed array issue and changed page formatting to condense view

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 Product.collection = [];
 var totalClicks = 0;
-var maxClicks = 25;
+var maxClicks = 5;
 var randomArray = [];
 var altRandomArray = [];
 
@@ -11,6 +11,8 @@ var priorVotes = JSON.parse(stringyPriorVotes);
 
 if(priorVotes){
   Product.collection = priorVotes;
+} else {
+  createProductLine();
 }
 
 // ===========================Basic Functions===========================
@@ -30,26 +32,28 @@ function Product(imgSrc, productName){
   Product.collection.push(this);
 }
 
-new Product('img/bag.jpg', 'Robot Suitcase');
-new Product('img/banana.jpg', 'Banana Slicer');
-new Product('img/bathroom.jpg', '2-in-1 Tablet Holder and Toilet Paper Dispenser');
-new Product('img/boots.jpg', 'Toeless Boots');
-new Product('img/breakfast.jpg', 'All in One Breakfast Maker');
-new Product('img/bubblegum.jpg', 'Meatball Bubblegum');
-new Product('img/chair.jpg', 'Standing Chair');
-new Product('img/cthulhu.jpg', 'Cthulhu Figurine');
-new Product('img/dog-duck.jpg', 'Dog Bill');
-new Product('img/dragon.jpg', 'Dragon Meat, 19oz');
-new Product('img/pen.jpg', 'Cutlery Pens');
-new Product('img/pet-sweep.jpg', 'Animal Powered Debris Removal System');
-new Product('img/scissors.jpg', 'Pizza Scissors');
-new Product('img/shark.jpg', 'Great White Sleeping Bag');
-new Product('img/sweep.png', 'Infant Powered Debris Removal System');
-new Product('img/tauntaun.jpg', 'Eviscerated Tauntaun Sleeping Bag');
-new Product('img/unicorn.jpg', 'Unicorn Meat, 5.5oz');
-new Product('img/usb.gif', 'Tentacle USB');
-new Product('img/water-can.jpg', 'Self-Watering Can');
-new Product('img/wine-glass.jpg', 'Terrarium Style Wine Glass');
+function createProductLine(){
+  new Product('img/bag.jpg', 'Robot Suitcase');
+  new Product('img/banana.jpg', 'Banana Slicer');
+  new Product('img/bathroom.jpg', '2-in-1 Tablet Holder and Toilet Paper Dispenser');
+  new Product('img/boots.jpg', 'Toeless Boots');
+  new Product('img/breakfast.jpg', 'All in One Breakfast Maker');
+  new Product('img/bubblegum.jpg', 'Meatball Bubblegum');
+  new Product('img/chair.jpg', 'Standing Chair');
+  new Product('img/cthulhu.jpg', 'Cthulhu Figurine');
+  new Product('img/dog-duck.jpg', 'Dog Bill');
+  new Product('img/dragon.jpg', 'Dragon Meat, 19oz');
+  new Product('img/pen.jpg', 'Cutlery Pens');
+  new Product('img/pet-sweep.jpg', 'Animal Powered Debris Removal System');
+  new Product('img/scissors.jpg', 'Pizza Scissors');
+  new Product('img/shark.jpg', 'Great White Sleeping Bag');
+  new Product('img/sweep.png', 'Infant Powered Debris Removal System');
+  new Product('img/tauntaun.jpg', 'Eviscerated Tauntaun Sleeping Bag');
+  new Product('img/unicorn.jpg', 'Unicorn Meat, 5.5oz');
+  new Product('img/usb.gif', 'Tentacle USB');
+  new Product('img/water-can.jpg', 'Self-Watering Can');
+  new Product('img/wine-glass.jpg', 'Terrarium Style Wine Glass');
+}
 
 console.log('Product Collection',Product.collection);
 
@@ -241,6 +245,13 @@ function printResults(){
 
 function renderResultsChart(){
 
+  var target = document.getElementById('intro');
+  var chart = document.createElement('canvas');
+  chart.id = 'myChart';
+  chart.width = '400px';
+  chart.height = '400px';
+  target.appendChild(chart);
+
   var chartLabels = [];
   for (var i = 0; i < Product.collection.length; i++){
     chartLabels.push(Product.collection[i].productName)
@@ -320,3 +331,5 @@ function saveToLocalStorage(){
   // }
 
 // }
+
+renderResultsChart();
